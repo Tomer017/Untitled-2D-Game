d0cfdcf0e7649badbdb726404e1379f05a1dc4a8
Tomer017 <vardi.tomer@gmail.com>
2025-05-22 09:01:46 -0700

    Add movement with the arrow keys

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,13 +7,6 @@ import javax.swing.JFrame;
  * Sets up the game window and initializes the game panel.
  */
 public class Main {
-    /**
-     * The main method that starts the game.
-     * Creates and configures the game window, adds the game panel,
-     * and starts the game thread.
-     *
-     * @param args Command line arguments (not used)
-     */
     public static void main(String[] args) {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
